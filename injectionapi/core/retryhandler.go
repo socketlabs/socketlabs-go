d724e500b87651fd458a6eb5a80534097d499e25
PraneethChandra <praneeth.chandra@socketlabs.com>
2021-04-12 10:07:56 -0700

    added retryhandler

--- injectionapi/core/retryhandler.go
+++ injectionapi/core/retryhandler.go
@@ -7,6 +7,36 @@ import (
 )
 
 type RetryHandler interface {
-	Send(client *http.Client, request *http.Request) (injectionapi.SendResponse, error)
+	Send(request *http.Request) (*http.Response, error)
 }
 
+type retryHandler struct {
+	HttpClient http.Client
+	EndpointUrl string
+	Settings injectionapi.RetrySettings
+}
+
+func CreateRetryHandler(client http.Client, endpointUrl string, settings injectionapi.RetrySettings) RetryHandler {
+	return &retryHandler{
+		HttpClient: client,
+		EndpointUrl: endpointUrl,
+		Settings: settings,
+	}
+}
+
+func (retryHandler *retryHandler) Send(request *http.Request) (*http.Response, error) {
+	if retryHandler.Settings.GetMaximumNumberOfRetries() == 0 {
+		response, err := retryHandler.HttpClient.Do(request)
+		if err != nil {
+			return nil, err
+		}
+
+		return response, nil
+	}
+	//attempts := 0
+	//for true {
+	//	waitInterval := retryHandler.Settings.GetNextWaitInterval(attempts)
+	//
+	//}
+	return nil, nil
+}
