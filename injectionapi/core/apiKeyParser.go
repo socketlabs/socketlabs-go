ae4d4a38cb63cb7e5b63fab96b4d624c04d2b6ba
mboshu <mike.boshuyzen@socketlabs.com>
2023-05-22 16:30:37 -0400

    Updated references and names

--- injectionapi/core/apiKeyParser.go
+++ injectionapi/core/apiKeyParser.go
@@ -2,29 +2,27 @@ package core
 
 import (
 	"strings"
-
-	"github.com/socketlabs/socketlabs-go/injectionapi/core/enums"
 )
 
 // Parses a provided api key and provides a result.
-type apiKeyParser struct{}
+type ApiKeyParser struct{}
 
-func (apiKeyParser) Parse(wholeApiKey string) (response enums.ApiParseResult) {
+func (parser ApiKeyParser) Parse(wholeApiKey string) (response string) {
 
 	if len(strings.TrimSpace(wholeApiKey)) == 0 {
-		response = InvalidEmptyOrWhitespace
+		response = "InvalidEmptyOrWhitespace"
 		return
 	}
 
 	if len(strings.TrimSpace(wholeApiKey)) != 61 {
-		response = InvalidKeyLength
+		response = "InvalidKeyLength"
 		return
 	}
 
 	splitIndex := strings.Index(wholeApiKey, ".")
 
 	if splitIndex == -1 || splitIndex > 50 {
-		response = InvalidKeyFormat
+		response = "InvalidKeyFormat"
 		return
 	}
 
@@ -33,7 +31,7 @@ func (apiKeyParser) Parse(wholeApiKey string) (response enums.ApiParseResult) {
 	publicPart := string(apiKeyRunes[0:splitIndex])
 
 	if len(strings.TrimSpace(publicPart)) != 20 {
-		response = InvalidPublicPartLength
+		response = "InvalidPublicPartLength"
 		return
 	}
 
@@ -42,10 +40,10 @@ func (apiKeyParser) Parse(wholeApiKey string) (response enums.ApiParseResult) {
 	secretPart := string(apiKeyRunes[splitIndex:len(wholeApiKey)])
 
 	if len(strings.TrimSpace(secretPart)) != 40 {
-		response = InvalidSecretPartLength
+		response = "InvalidSecretPartLength"
 		return
 	}
 
-	response = Success
+	response = "Success"
 	return
 }
